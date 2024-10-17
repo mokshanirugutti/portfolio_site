70a9a24fdbce4811873d00fdd4dcd7657406c8e6
mokshanirgutti <mokshasai910@gmail.com>
2024-10-17 19:12:01 +0530

    padding issues

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -2,7 +2,7 @@ import HeroSectionRevealAnimation from "./HeroSectionRevealAnimation";
 
 export default function HeroSection() {
     return (
-    <div className="bg-zinc-100">
+    <div className="bg-zinc-100 py-1">
       <HeroSectionRevealAnimation/>
     </div>
     
--- components/HeroSection/IntroDoc.tsx
+++ components/HeroSection/IntroDoc.tsx
@@ -1,13 +1,14 @@
-import WordRotate from "../ui/word-rotate";
+// import WordRotate from "../ui/word-rotate";
 import { SocialsDoc } from "./SocialsDoc";
 
 export default function IntroDoc() {
     return (
-        <div className="border border-teal-200 rounded-3xl shadow-md px-7 py-3 flex justify-between items-center w-[85%]  mx-auto flex-wrap">
+        <div className="border border-teal-200 rounded-3xl shadow-md px-10 py-1 flex justify-between items-center w-[85%]  mx-auto flex-wrap">
             <div className="flex flex-col gap-1">
                 <h1 className="text-3xl font-bold ">Moksha Nirugutti</h1>
+                <h1 className="text-xl font-bold ">FullStack Developer</h1>
                 
-                    <WordRotate words={['FrontEnd Developer','Backend Developer' ,'FullStack Developer']} className="text-lg font-bold "/>
+                    {/* <WordRotate words={['FrontEnd Developer','Backend Developer' ,'FullStack Developer']} className="text-lg font-bold "/> */}
                      
             </div>
             <div className="">
